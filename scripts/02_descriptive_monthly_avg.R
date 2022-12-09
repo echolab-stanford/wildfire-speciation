b90fed9ee52546dcbda0b192b683e2d92d10f1d3
ayako-kawano <81779718+ayako-kawano@users.noreply.github.com>
2022-12-09 13:50:28 -0800

    update scripts

--- scripts/02_descriptive_monthly_avg.R
+++ scripts/02_descriptive_monthly_avg.R
@@ -9,14 +9,14 @@ library(anytime)
 library(tidyr)
 
 #setup
-setwd("C:/Users/ayako/OneDrive/Documents/echolab/wip-ayako/wildfire-speciation/scripts")
+setwd("C:/Users/ayako/Documents/StanfordPhD/EchoLab/wildfire-speciation/scripts")
 data_path <- "G:/Shared drives/echolab data/wildfire_speciation/intermediate/"
-fig_path <- "C:/Users/ayako/OneDrive/Documents/echolab/wip-ayako/wildfire-speciation/figures"
+fig_path <- "C:/Users/ayako/Documents/StanfordPhD/EchoLab/wildfire-speciation/figures"
 
 #import data
-data <- read.csv(paste(data_path, "pm_plume_speciation_at_sites.csv", sep = ''))
+data <- read.csv(paste(data_path, "pm_plume_speciation_at_sites_tempAK.csv", sep = ''))
 #file includes row names, so drop the column 
-data <- data[,-1]
+#data <- data[,-1]
 data$smoke_day <- as.factor(data$smoke_day)
 #add a short region name
 data <- data %>% 
@@ -35,16 +35,15 @@ data$Date <- as.Date(data$Date)
 class(data$Date)
 
 data$month_yr <- format(data$Date, "%Y-%m")
-monthly <- data %>% select(month_yr, year, month, RC_PM2, NH4, tot_metals, ammNO3,ammSO4,
-                        AL, AS, BR, CA, CR, CU, FE, K, MG, MN, NI,
-                        NO3, N2, P, PB, RB, S, SE, SI, SOIL, SO4,
-                        SR, V, ZN, ZR, MF)
+monthly <- data %>% select(month_yr, year, month, MF, RC_PM2, NH4, tot_metals, 
+                           ammNO3, ammSO4, AL, AS, BR, 
+                           CA, EC, OC, CHL, CL, CR, CU, FE, K, MG, MN, MO, NA., NI,
+                           NO3, N2, P, PB, RB, S, SE, SI, SOIL, SO4, SR, V, ZN, ZR)
 
-vars <- data %>% select(RC_PM2, NH4, tot_metals, ammNO3,ammSO4,
-                        AL, AS, BR, CA, CR, CU, FE, K, MG, MN, NI,
-                        NO3, N2, P, PB, RB, S, SE, SI, SOIL, SO4,
-                        SR, V, ZN, ZR, MF) %>% names()
-
+vars <- data %>% select(MF, RC_PM2, NH4, tot_metals, ammNO3, ammSO4, AL, AS, BR, 
+                        CA, EC, OC, CHL, CL, CR, CU, FE, K, MG, MN, MO, NA., NI,
+                        NO3, N2, P, PB, RB, S, SE, SI, SOIL, SO4, SR, V, ZN, ZR
+                        )%>% names()
 
 
 #create monthly data, calculate monthly mean
@@ -62,34 +61,30 @@ monthly_gb$month <- factor(monthly_gb$month, levels=c("1", "2", "3",
                                                       "10", "11", "12"))
 
 
-#all region, time series of monthly means from 2006-2021
+#all region, time series of monthly means from 2006-2020
 for(i in vars) {
   
   g <- ggplot(monthly_gb, aes(x = month_yr, y = get(i))) +
     geom_point(na.rm=TRUE, color="darkblue", size=1) +
-    (scale_x_date(breaks=date_breaks("12 months"),
-                  labels=date_format("%y"))) +
+    #(scale_x_date(breaks=date_breaks("12 months"),
+    #              labels=date_format("%y"))) +
     ylab(i) +
     xlab("Year") +
-    ggtitle("Monthly means, 2006-2021")
+    ggtitle("Monthly means, 2006-2020")
   print(g)
   ggsave(path = paste(fig_path,"/09_monthly_means",sep = ''), 
          filename = paste0(i, ".png"), device = "png",
          width = 5, height = 3, dpi = 300)
 }
 
-#all region, seasonality plot of monthly means from 2006-2021
-ggplot(monthly_gb, aes(x = month, y = RC_PM2, group = year, color = year)) +
-  geom_line()
-
-#all region, time series of monthly means from 2006-2021
+#all region, time series of monthly means from 2006-2020
 for(i in vars) {
   
   g <- ggplot(monthly_gb, aes(x = month, y = get(i), group = year, color = year)) +
     geom_line() +
     ylab(i) +
     xlab("Month") +
-    ggtitle("Monthly means, 2006-2021")
+    ggtitle("Monthly means, 2006-2020")
   print(g)
   ggsave(path = paste(fig_path,"/10_monthly_means_seasonality",sep = ''), 
          filename = paste0(i, ".png"), device = "png",
